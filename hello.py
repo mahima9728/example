86247830c0e73509c476e6257e2dfa2a149c462e
mahima9728 <128793921+mahima9728@users.noreply.github.com>
2023-04-11 16:04:20 +0530

    update hello from num to name

--- hello.py
+++ hello.py
@@ -1,9 +1,2 @@
 name = input("Enter your name : ")
 print("HELLO ", name.upper())
-num = int(input("Enter a number : "))
-if num > 0:
-    print("Number is positive")
-elif num < 0:
-    print("Number is negative")
-else:
-    print("Number is zero")
